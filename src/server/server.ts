46b64445d6a645cb38c01cdff8a93c0c744a0ffc
Ray C <raychanray64@gmail.com>
2024-08-15 13:43:54 -0400

    "Updated package-lock.json, added ejs dependency, modified package.json, and created an express server with EJS templating engine in server.ts"

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,21 @@
-import * as express from "express";
+import express from "express";
 
-console.log(express);
+const server = express();
+
+//middleware
+server.use(express.static("dist"));
+
+//use EJS templating engine for Node. js that allows you to generate HTML markup with plain JavaScript.
+server.set("view engine", "ejs");
+
+
+//routes
+server.use("/", (req, res) => {
+    res.render("index", {
+        title: "NAMING CONTEST REPO",
+    });
+})
+
+server.listen("8080", "0.0.0.0", () => {
+    console.info("Listening on port http://0.0.0.0:8080");
+})
